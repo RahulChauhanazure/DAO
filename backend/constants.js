dc0b0e805bd374f22ef800932551f3f0c56b6325
Rahul Chauhan <rahulchauhan2k2@yahoo.com>
2023-02-08 21:28:45 -0500

    Initial Commit

---
+++ backend/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x80195AC099dB3e4c4DD643E4051F0fDe2f5c94da";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
